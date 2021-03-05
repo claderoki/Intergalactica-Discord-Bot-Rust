562e779882ff2b7a3b9bae34b72a010994765fb1
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-03-05 18:11:54 +0100

    default

--- src/modules/conversion/currency.rs
+++ src/modules/conversion/currency.rs
@@ -1,7 +1,21 @@
+use std::{collections::HashMap, env};
 
+use super::super::super::wrappers::fixerio::api::Fixerio;
 
+pub async fn get_rates() -> Result<HashMap<String, f64>, &'static str>{
+    let fixerio = Fixerio::new(env::var("FIXERIO_ACCESS_KEY").expect("No fixerio access key set."));
+    let rates = fixerio.get_rates().await;
 
+    match rates {
+        Ok(data) => {
+            return Ok(data.rates);
+        }
+        Err(e) => {
+            return Err(e)
+        }
+    }
+}
 
 pub fn convert(from : String, to : String) {
-
+    let rates = get_rates();
 }
