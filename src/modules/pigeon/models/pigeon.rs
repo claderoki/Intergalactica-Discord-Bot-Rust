67b6b97505d51e9be61df652fbcfa2806c39fb54
Claderoki <c.derhaag@protonmail.com>
2021-03-21 23:22:43 +0100

    pigeon model initialization

---
+++ src/modules/pigeon/models/pigeon.rs
@@ -0,0 +1,35 @@
+
+enum PigeonStatus {
+    IDLE,
+    MAILING,
+    EXPLORING,
+    FIGHTING,
+    DATING,
+}
+
+enum PigeonCondition {
+    ACTIVE,
+    RAN_AWAY,
+    DEAD,
+}
+
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER,
+}
+
+
+struct Pigeon {
+    pub id: i32,
+    pub name: String,
+    pub human_id: i32,
+    pub condition: PigeonCondition,
+    pub experience: i32,
+    pub cleanliness: i32,
+    pub happiness: i32,
+    pub food: i32,
+    pub health: i32,
+    pub status: PigeonStatus,
+    pub gender: Gender,
+}
