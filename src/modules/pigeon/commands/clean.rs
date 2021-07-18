1f34a471671c09c87c5e9bda493e3611ceb6e38c
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-07-18 13:16:40 +0200

    bucket experiment

--- src/modules/pigeon/commands/clean.rs
+++ src/modules/pigeon/commands/clean.rs
@@ -13,6 +13,55 @@ use crate::modules::shared::caching::flag::FlagCache;
 use crate::modules::shared::caching::flag::FlagValidator;
 use crate::modules::shared::caching::flag::PigeonLastCleaned;
 
+pub struct GenericFlag<T> where T: ToString {
+    pub when: NaiveDateTime,
+    pub identifier: T,
+}
+
+impl Flag for GenericFlag {
+    fn get_key(&self) -> String {
+        self.identifier
+    }
+
+    fn new(when: NaiveDateTime) -> Self {
+        Self {
+            when,
+            identifier
+        }
+    }
+}
+
+enum BucketType {
+    Guild(u64),
+    Member(u64, u64),
+    User(u64),
+}
+
+struct Bucket {
+    pub identifier: String,
+    pub bucket_type: BucketType,
+    pub cooldown: std::time::Duration,
+}
+
+impl Bucket {
+    pub fn new(identifier: &'static str, bucket_type: BucketType, cooldown: std::time::Duration) -> Self {
+        Self {
+            identifier: identifier.into()
+            bucket_type,
+            cooldown
+        }
+    }
+
+    pub fn validate() -> Result<(), String> {
+        Ok(())
+    }
+
+    pub fn spend() {
+
+    }
+
+}
+
 #[command("clean")]
 #[description("Clean your pigeon.")]
 pub async fn clean(ctx: &Context, msg: &Message) -> CommandResult {
@@ -30,7 +79,9 @@ pub async fn clean(ctx: &Context, msg: &Message) -> CommandResult {
         return Err("You already have max cleanliness!".into());
     }
 
-    let now = FlagValidator::validate::<PigeonLastCleaned>(human_id, Duration::minutes(45))?;
+    let bucket = Bucket::new("pigeon_clean", BucketType::User(msg.author.id.into()), std::time::Duration::from_mins(45));
+    // let now = FlagValidator::validate::<PigeonLastCleaned>(human_id, Duration::minutes(45))?;
+    bucket.validate()?;
 
     let winnings = PigeonWinningsBuilder::new()
         .cleanliness(increase)
@@ -46,6 +97,7 @@ pub async fn clean(ctx: &Context, msg: &Message) -> CommandResult {
     .await?;
     PigeonRepository::update_winnings(human_id, &winnings)?;
 
-    FlagCache::add::<PigeonLastCleaned>(human_id, now);
+    // FlagCache::add::<PigeonLastCleaned>(human_id, now);
+    bucket.spend();
     Ok(())
 }
