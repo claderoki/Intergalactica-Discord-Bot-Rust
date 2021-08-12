07fc1e5249b54eea8d6b61890263f0492ebf424f
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-08-12 20:47:47 +0200

    cleanup

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -2,18 +2,6 @@ use diesel::prelude::Connection;
 use diesel::prelude::MysqlConnection;
 use std::env;
 
-// const fn abc() -> String {
-//     format!(
-//         "mysql://{}:{}@{}/{}",
-//         env::var("DB_USER").expect("Expected DB_USER in the environment"),
-//         env::var("DB_PASSWORD").expect("Expected DB_PASSWORD in the environment"),
-//         env::var("DB_HOST").expect("Expected DB_HOST in the environment"),
-//         env::var("DB_NAME").expect("Expected DB_NAME in the environment"),
-//     )
-// }
-
-// static DB_URL: &str = ;
-
 fn get_db_url() -> String {
     format!(
         "mysql://{}:{}@{}/{}",
