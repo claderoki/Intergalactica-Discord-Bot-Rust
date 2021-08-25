cbcc9fa07f92452455a7fe7107a0935d2d83d65f
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-08-25 07:55:38 +0200

    syntax fix

--- src/modules/pigeon/repository/queries/exploration/random_location.sql
+++ src/modules/pigeon/repository/queries/exploration/random_location.sql
@@ -3,7 +3,7 @@ SELECT
     IFNULL(`exploration_planet_location`.`image_url`, `exploration_planet`.`image_url`) as image_url,
     90 as `travel_distance_in_minutes`
 FROM
-    `exploration_planet_location`x
+    `exploration_planet_location`
 INNER JOIN `exploration_planet` ON `exploration_planet`.`id` = `exploration_planet_location`.`planet_id`
 WHERE
     `exploration_planet`.`id` != 1
